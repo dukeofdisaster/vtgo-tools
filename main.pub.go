84e896cd8bc0a0c6cde5eb4cc8fd156187021420
dukeofdisaster <ortizzlejay@gmail.com>
2019-05-03 13:59:16 -0700

    Implemented url check / pivot to scan for old scans.. TODO: read from file and submit file

--- main.pub.go
+++ main.pub.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	st "strings"
 	"time"
 
@@ -71,17 +72,17 @@ func main() {
 	}
 	// We can instantiate a client & scanner  outside of the rest of the logic,
 	//since we'll them one regardless
-	client := vt.NewClient("YOURAPIKEYHERE")
+	client := vt.NewClient("YOUR API KEY HERE")
 	scanner := client.NewURLScanner()
 
 	if len(os.Args) == 2 {
-		fmt.Println("2 args: program and one other")
+		//fmt.Println("2 args: program and one other")
 		_singleArg := os.Args[1]
 
 		// (1) - Validate the URL has a protocol identifier; if not, add it
 		if !(st.Contains(_singleArg, "http://") || st.Contains(_singleArg, "https://")) {
 			_singleArg = "http://" + _singleArg
-			fmt.Printf("\nNew String: %s", _singleArg)
+			//fmt.Printf("\nNew String: %s", _singleArg)
 		}
 
 		// we won't need a scan scanner unless the last scan date is too old.
@@ -89,11 +90,13 @@ func main() {
 		// (2) - supplied url will be identified by it's b64 encoded counterpart
 		//      in VT data
 		var urlID = base64.RawURLEncoding.EncodeToString([]byte(_singleArg))
-		fmt.Println(urlID)
+		//fmt.Println(urlID)
 
 		// (3) - Build a VT URL  out the urlID
 		var noscanURL = vt.URL("urls/%s", urlID)
 
+		// build a URL for scans
+		//var newscanURL = vt.URL()
 		// (?) - with no scan we can use a simple GET
 		rawresponse, err := client.Get(noscanURL)
 		if err != nil {
@@ -101,7 +104,7 @@ func main() {
 		}
 
 		// Dump the json
-		fmt.Printf("%s", rawresponse.Data)
+		//fmt.Printf("%s", rawresponse.Data)
 
 		// try to unmarshal the data into the var;
 		// this kinda works but we have to use ugly type assertions to get
@@ -126,57 +129,82 @@ func main() {
 
 		// we cast the analysis date to int64 to avoid format printing of float.
 		// if the last analysis date < our 30 day window time, we rescan, else, we collect data
-		if lastanal < thirtydaysago {
-			fmt.Println("Smaller")
-			//fmt.Printf("\n30dayWindow: %d\n",epochnow)
-			// TODO: Enter scan logic here
-		} else {
-			fmt.Printf("\nFreshly Scanned: ")
+		if lastanal <= thirtydaysago {
+			fmt.Printf("\nOld Scan: ")
 			fmt.Println(time.Unix(lastanal, 0))
-			fmt.Printf("30dayWindow: %d\n", thirtydaysago)
+			fmt.Println("Submitting URL for fresh scan...")
+
+			// Scanner has already been instantiated by this point... here we take
+			// the URL id and scan it.. this will return  an analysis object...
+			// we use the analasysobject ID to get the results of the scan
+			analobject, err := scanner.Scan(_singleArg)
+			if err != nil {
+				log.Fatal(err)
+				fmt.Println("Err scanning object... exit 1")
+				os.Exit(1)
+			}
+			var analysisID = analobject.ID
+			fmt.Println("Analysis Object ID: " + analysisID)
+			fmt.Println("Sleep for scan; max 10 seconds...")
+
+			// time library requires you to pass an increment object * integer amount
+			//time.Sleep(1 * time.Second)
+			var resultsURL = vt.URL("analyses/%s", analysisID)
+			resultsData, err := client.GetObject(resultsURL)
+			if err != nil {
+				log.Fatal(err)
+				fmt.Println("Err in GetObject() on fresh scan")
+				os.Exit(1)
+			}
+			//fmt.Println(resultsData)
+
+			// Below will print as json.Number; havet o convert to Float64 -> Int64 -> Timestamp
+			//fmt.Println(resultsData.Attributes["date"])
+			newdateFloat, err := resultsData.Attributes["date"].(json.Number).Float64()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// convert new date to int64 so it'll print nicely when we pass it to Println
+			newdate := int64(newdateFloat)
+			fmt.Printf("New Scan Date: ")
+			fmt.Println(time.Unix(newdate, 0))
+			// S
+			fmt.Printf("Malicious: %s\n", resultsData.Attributes["stats"].(map[string]interface{})["malicious"])
+			fmt.Printf("Harmless: %s\n", resultsData.Attributes["stats"].(map[string]interface{})["harmless"])
+			// Note the link Id will always be the id of the url.. since this is a scan ID
+			// we have to split it by '-'; the id we want will always be the 2nd item in the split
+			strippedID := strings.Split(analysisID, "-")
+			var newscanurl = "https://www.virustotal.com/#/url/" + strippedID[1] + "/detection"
+			fmt.Println("Link: " + newscanurl)
+			os.Exit(1)
+		} else {
+			fmt.Printf("Freshly Scanned: ")
+			fmt.Println(time.Unix(lastanal, 0))
+			// Pull out the last_analysis_stats object
+			var statsobj = dat["attributes"].(map[string]interface{})["last_analysis_stats"]
+
+			// from he last_analysis_stats object, access the value of the "malicious" key..
+			// note we assert that it's a float, then cast the float to an in64 to avoid shitty ypriinting
+			var maliciouscount = int64(statsobj.(map[string]interface{})["malicious"].(float64))
+			var harmlesscount = int64(statsobj.(map[string]interface{})["harmless"].(float64))
+
+			//fmt.Println(statsobj)
+			fmt.Printf("Malicious Hits: %d\n", maliciouscount)
+			fmt.Printf("Harmless Count: %d\n", harmlesscount)
+
+			// We'll extract the ID too make a plain URL out of since
+			// the link returned in the json is only for the api... note this is an outer
+			// object of the data so we can access it more easily
+			var id = dat["id"].(string)
+
+			// basic url detection syntax is vt.com/#/url/<ID>/detection:
+			var detectionurl = "https://www.virustotal.com/#/url/" + id + "/detection"
+			fmt.Println("Link: " + detectionurl)
 		}
-
-		//*
-		// (?-1) Test with getobject to acces inner values
-		//rawData, err := client.GetObject(noscanURL)
-		//handle_it(err)
-		// we want to check last_analysis_date
-		// 30 days in epoch = 2592000
-		// 1 day = 86400
-		//last_scan, err := rawData.Attributes["last_analysis_date"].(json.Number).Float64()
-		//handle_it(err)
-		//fmt.Println(rawData.Attributes)
-		//*/
-
-		//handle_it(err1)
-		//fmt.Printf("Last scan %d", last_scan)
-		//fmt.Printf("\nLast_Scan2: %s", last_scan2)
-		//if int64(last_scan) < (int64(time.Now().Unix()) - int64(86400)) {
-		//  fmt.Println("last_analysis is numeric")
-		//}
 
 		// Interestingly enough this value will automatically get printed as an actual date
 		//fmt.Printf("Object Attributes: %s", rawData.Attributes["last_analysis_date"])
-		os.Exit(1)
-
-		// After we've validated the URL we can get an analysis object for the url by scanning it
-		analobject, err := scanner.Scan(_singleArg)
-		if err != nil {
-			log.Fatal(err)
-			fmt.Println("Err scanning object... exit 1")
-			os.Exit(1)
-		}
-
-		// after we've gotten the analysis object it will have an ID; this is used
-		// to get the data about the url
-		// TODO: Add functionality to rescan if the last scan date is too old
-		var id_url = vt.URL("analyses/%s", analobject.ID)
-		jdata, err := client.GetObject(id_url)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("\nMalicious: %s", jdata.Attributes["stats"].(map[string]interface{})["malicious"])
-		fmt.Printf("Stats %s", jdata.Attributes["stats"])
 		os.Exit(0)
 	}
 
@@ -188,92 +216,4 @@ func main() {
 		fmt.Println("No file given")
 	}
 
-	// If we just want to scan one url we don't need a flag for that
-
-	// We should check if there's a protocol identifier; if not add one because we don't want
-	// to check domains...
-	// TODO: Add functionality for pulling down domain relations with url identification
-	//      - NOTE: that should probably only be done when we're given a file to parse
-	//fmt.Println(_singleArg)
-	//flag.Parse()
-
-	// only for scanning fresh urls; these analysis ID's aren't
-
-	// Scanner returns an analysys object; this will have an ID attributue
-	// which we can then use to get the analyses
-	//analobject, err := scanner.Scan("www.google.com")
-	analobject, err := scanner.Scan("http://www.pepperbagz.com/wp-content/themes/basel/fonts/1c.jpg")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("RESULT: %s", analobject.ID)
-	var id_url = vt.URL("analyses/%s", analobject.ID)
-
-	// This will return a response object which
-	// we can pass directly as a string, or access parts of. the struct is
-	// defined as such in vt.go
-	/*
-
-		// Response is the top level structure of an API response.
-		type Response struct {
-		  Data  json.RawMessage        `json:"data"`
-		  Meta  map[string]interface{} `json:"meta"`
-		  Links Links                  `json:"links"`
-		  Error Error                  `json:"error"`
-		}
-
-	*/
-	// the raw data must still be unmarshalled... for a higiher level
-	// functionality ywe must use GetData and send the unmarshalled data to a structure
-	// returns vt.Response of type byte[]
-	/*
-		resp2, err := client.Get(id_url)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("\n\n\nRAN WITH client.Get() - prints resp2.Data: %s", resp2.Data)
-	*/
-	// Returns an object
-	data1, err := client.GetObject(id_url)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// since the type of data1.Attributes["stats"] is of type map[string]interface{}
-	// to access it we have to acess the value in this way
-	fmt.Printf("Ran with client.GetObject(): %s", data1.Attributes["stats"].(map[string]interface{})["malicious"])
-
-	// try to use the value in logic
-	// The json object has type json.Number, which must be converted to numeric
-	// data type Float64() to be used in arithmetic
-	zero, err := data1.Attributes["stats"].(map[string]interface{})["malicious"].(json.Number).Float64()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if zero == 0 {
-		fmt.Println("True")
-	} else {
-		fmt.Println("FALSE")
-	}
-	// here we use a different method by unmarshalling the data without specifying
-	// the structure
-	// recall that we get errors because we have a type interface{} that doesn't jive
-	// with string or int or whatever
-	//m := make(map[string]interface{})
-	//err := json.Unmarshal(resp2, &m)
-	//if err != nil {
-	//  log.Fatal(err)
-	//}
-	//fmt.Println(m["malicious"])
-	//var resp3 ResponseDict
-	/*
-		var interf interface{}
-		data2, err := client.GetData(id_url, &interf)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("Ran with client.GetData()\n========%s\n========", interf)
-		fmt.Printf("data2: %s", data2)
-	  /**/
-	// Now that we've received a response we can parse this with response
 }
